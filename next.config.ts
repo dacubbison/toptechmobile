a3a3073608f180965d9346ff2e88c12230d5c5a6
David Cubbison <david@toptechmobile.com>
2026-02-20 13:46:03 -0600

    Final cleanup: remove empty app/api folder (Twilio is officially dead forever)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
+// next.config.ts
 const nextConfig = {
   images: {
     remotePatterns: [],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',   // allows big photos & PDFs
+    },
   },
 };
 
